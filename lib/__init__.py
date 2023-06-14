71a37d5275aa13d03f9c676d3baf831c39f0f8aa
liwuchao <liwuchao@mail.ustc.edu.cn>
2023-06-14 10:58:05 +0800

    fix ndcg bug

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,5 @@
 from .Trm4Rec_trainer import Trm4Rec
-from .generate_training_batches import Train_instance
 from .DIN_trainer import DINTrain
 from .DIN_Model import DeepInterestNetwork
+from .JTM_variant import JTM_Variant
+from .generate_training_batches import Train_instance
